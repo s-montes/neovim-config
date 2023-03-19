34367f3ed322d6ffe0e4cccc0f5bfdb4678100a7
S. Montes <montes.valencia.sebastian@gmail.com>
2023-03-19 17:33:55 +0100

    Mappings for switching buffers

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,14 @@ end
 return {
   -- first key is the mode
   n = {
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
     ["<leader>y"] = { '"+y', desc = "Yank to clipboard" },
     ["<leader>p"] = { '"+p', desc = "Paste from clipboard" },
     -- second key is the lefthand side of the map
