7aface7424a9e7b7d4a066735c0deff1281379d1
S. Montes <montes.valencia.sebastian@gmail.com>
2023-03-31 21:11:15 +0200

    Change splash screen

--- plugins/themes.lua
+++ plugins/themes.lua
@@ -19,4 +19,33 @@ return {
   {
     "EdenEast/nightfox.nvim",
   },
+  {
+    "goolord/alpha-nvim",
+    opts = function()
+      local dashboard = require "alpha.themes.dashboard"
+      local pwd = vim.fn.getcwd()
+      local home = os.getenv "HOME" .. "/"
+      pwd = pwd:gsub(home, "")
+      dashboard.section.header.val = {
+        "    ███    ██ ███████ ███████ ██    ██ ██ ███    ███",
+        "    ████   ██ ██      ██   ██ ██    ██ ██ ████  ████",
+        "    ██ ██  ██ ███████ ██   ██ ██    ██ ██ ██ ████ ██",
+        "    ██  ██ ██ ██      ██   ██  ██  ██  ██ ██  ██  ██",
+        "    ██   ████ ███████ ███████   ████   ██ ██      ██",
+        "",
+        "",
+        string.format("    %s", pwd),
+      }
+      local button = require("astronvim.utils").alpha_button
+      dashboard.section.buttons.val = {
+        button("LDR n", "  New File  "),
+        button("LDR f f", "  Find File  "),
+        button("LDR f o", "  Recents  "),
+        button("LDR f w", "  Find Word  "),
+        button("LDR f '", "  Bookmarks  "),
+        button("LDR S f", "  Find Session  "),
+      }
+      return dashboard
+    end,
+  },
 }
